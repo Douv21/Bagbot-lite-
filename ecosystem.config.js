51a21a55c06152f08c10abad39c1533fe149f413
Douv21 <douv21@example.com>
2026-04-27 21:17:21 +0200

    Add ecosystem.config.js for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bagbot-dashboard',
+      script: './src/dashboard.js',
+      cwd: '/home/maison/bagbot',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 49501
+      }
+    }
+  ]
+};
